bd094bbd6549c7ec30890f91462716d5e0c54907
awakn123 <awakn123@gmail.com>
2017-05-04 12:22:14 +0800

    引入拼音处理

---
+++ src/main/java/pinyin/PinYin.java
@@ -0,0 +1,22 @@
+package pinyin;
+
+import SqlToMysql.util.ListUtils;
+import com.google.common.collect.Lists;
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinVCharType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+public class PinYin {
+
+	public static void main(String[] args) throws BadHanyuPinyinOutputFormatCombination {
+		HanyuPinyinOutputFormat pyFormat = new HanyuPinyinOutputFormat();
+		pyFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+		pyFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+		pyFormat.setVCharType(HanyuPinyinVCharType.WITH_V);
+		String[] result = PinyinHelper.toHanyuPinyinStringArray('潇', pyFormat);
+		System.out.println(ListUtils.toString(Lists.newArrayList(result)));
+	}
+}
