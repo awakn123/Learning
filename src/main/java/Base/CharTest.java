90fc99592e906489f1f67ca268d133a2b9793bfc
awakn123 <awakn123@gmail.com>
2017-05-24 13:30:45 +0800

    删除CharTest中的无用代码。 增加16进制转义测试

--- src/main/java/Base/CharTest.java
+++ src/main/java/Base/CharTest.java
@@ -2,20 +2,6 @@ package Base;
 
 public class CharTest {
 	public static void main(String[] args) {
-		// char码值对应列表
-		// http://www.cnblogs.com/tian_z/archive/2010/08/06/1793736.html
-		/*for (int i=0;i<133;i++) {
-			char a = (char)i;
-			System.out.printf("Char(%s):%s;\n",i,a);
-		}*/
-		/*char i = 32;
-		System.out.println("32:"+i+";");
-		i = 9;
-		System.out.println("9:"+i+";");
-		String space = " ";
-		String tab = "	";
-		System.out.println("space=32:" + ((space.charAt(0)) == (char) 32));
-		System.out.println("tab=9:" + ((tab.charAt(0)) == (char) 9));*/
 		numToChar(39);
 		charToNum('\'');
 	}
---
+++ src/main/java/Base/HexTest.java
@@ -0,0 +1,9 @@
+package Base;
+
+public class HexTest {
+	public static void main(String[] args) {
+		char a = 0x07;
+		System.out.println(a);
+	}
+
+}
