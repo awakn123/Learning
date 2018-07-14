d733a0f386343b4fe52207334aa2318bec034525
awakn123 <awakn123@gmail.com>
2018-07-14 11:25:09 +0800

    提交Stream测试

---
+++ src/main/java/Base/StreamTest.java
@@ -0,0 +1,28 @@
+package Base;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StreamTest {
+    public static void main(String[] args) {
+        List<String> ids = Lists.newArrayList("1", "2", "3", "4");
+        System.out.println("reduce:"+ids.stream().reduce((acc, item) -> acc+"," + item).get());
+        System.out.println("collect:"+ids.stream().collect(Collectors.joining()));
+        System.out.println("collect ,:"+ids.stream().collect(Collectors.joining(",")));
+        System.out.println("collect prefix&&suffix:"+ids.stream().collect(Collectors.joining(",", "pre-", "-suf")));
+        System.out.println("new collect:"+ ids.stream().collect(Collector.of(
+                () -> new StringBuilder(),
+                StringBuilder::append,
+                StringBuilder::append
+        )));
+        System.out.println("collect ?:"+ ids.stream().collect(Collector.of(
+                ()-> new StringBuilder(),
+                (builder, item)-> builder.append("?,"),
+                (builder, builder2)-> builder.append(builder2)
+        )) + "; wrong");
+        System.out.println("join ?:" + ids.stream().map((item)-> "?").collect(Collectors.joining(",")));
+    }
+}
