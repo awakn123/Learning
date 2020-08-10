eb0508811a337be6df3432eacf252c05268248d1
awakn123 <awakn123@gmail.com>
2020-08-10 17:23:37 +0800

    #leetcode# reverse integer.

--- src/main/java/leetcode/easy/string/ReverseInteger.java
+++ src/main/java/leetcode/easy/string/ReverseInteger.java
@@ -2,14 +2,30 @@ package leetcode.easy.string;
 
 import leetcode.util.ResultCheck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 曹云 on 2020/8/6.
  * 7. Reverse Integer
  * https://leetcode.com/problems/reverse-integer/
  */
 public class ReverseInteger {
+	/**
+	 * see and reimpl.
+	 * @param x
+	 * @return
+	 */
 	public int reverse(int x) {
-		return 0;
+		int y =0;
+		while(x!=0) {
+			int pop = x%10;
+			x /= 10;
+			if (y > Integer.MAX_VALUE/10 || (y == Integer.MAX_VALUE/10 && pop > 7)) return 0;
+			if (y < Integer.MIN_VALUE/10 || (y == Integer.MIN_VALUE/10 && pop < -8)) return 0;
+			y = y * 10 + pop;
+		}
+		return y;
 	}
 	public static void main(String[] args){
 		ReverseInteger main = new ReverseInteger();
@@ -25,5 +41,11 @@ public class ReverseInteger {
 		int answerIII = 0;
 		int resultIII = main.reverse(xIII);
 		ResultCheck.check(resultIII, answerIII);
+		int xIV = 1534236469;
+		int answerIV = 0;
+		int resultIV = main.reverse(xIV);
+		ResultCheck.check(resultIV, answerIV);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 	}
 }
