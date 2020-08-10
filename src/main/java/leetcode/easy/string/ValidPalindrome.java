74777547157441845835d1c27f8a7f10c9400a68
awakn123 <awakn123@gmail.com>
2020-08-10 19:30:00 +0800

    #leetcode# validpalindrome

--- src/main/java/leetcode/easy/string/ValidPalindrome.java
+++ src/main/java/leetcode/easy/string/ValidPalindrome.java
@@ -2,6 +2,9 @@ package leetcode.easy.string;
 
 import leetcode.util.ResultCheck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 曹云 on 2020/8/6.
  * 125. Valid Palindrome
@@ -10,7 +13,16 @@ import leetcode.util.ResultCheck;
 public class ValidPalindrome {
 
 	public boolean isPalindrome(String s) {
-		return false;
+		int l = 0, r = s.length() - 1;
+		while (l < r) {
+			while (l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+			while (l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+			if (l < r && Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+				return false;
+			l++;
+			r--;
+		}
+		return true;
 	}
 
 	public static void main(String[] args){
@@ -23,5 +35,9 @@ public class ValidPalindrome {
 		boolean answerII = false;
 		boolean resultII = main.isPalindrome(sII);
 		ResultCheck.check(resultII, answerII);
+		String sIII = "\"0P\"";
+		boolean answerIII = false;
+		boolean resultIII = main.isPalindrome(sIII);
+		ResultCheck.check(resultIII, answerIII);
 	}
 }
