ef798499fd62c539dcd75e9d8b8cac621ca72c8c
awakn123 <awakn123@gmail.com>
2020-08-11 14:46:04 +0800

    #leetcode# longest common prefix.

--- src/main/java/leetcode/easy/string/LongestCommonPrefix.java
+++ src/main/java/leetcode/easy/string/LongestCommonPrefix.java
@@ -9,7 +9,17 @@ import leetcode.util.ResultCheck;
  */
 public class LongestCommonPrefix {
 	public String longestCommonPrefix(String[] strs) {
-		return "";
+		if (strs.length == 0) return "";
+		if (strs.length == 1) return strs[0];
+		StringBuilder out = new StringBuilder();
+		for (int i=0; i<strs[0].length(); i++) {
+			for (int j=1; j<strs.length; j++) {
+				if (i >= strs[j].length() || strs[0].charAt(i) != strs[j].charAt(i))
+					return out.toString();
+			}
+			out.append(strs[0].charAt(i));
+		}
+		return out.toString();
 	}
 	public static void main(String[] args){
 		LongestCommonPrefix main = new LongestCommonPrefix();
