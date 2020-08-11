6fde8d68a501899570648d445f1e5416101cd575
awakn123 <awakn123@gmail.com>
2020-08-11 13:52:49 +0800

    #leetcode# Implement strStr()

--- src/main/java/leetcode/easy/string/ImplStrStr.java
+++ src/main/java/leetcode/easy/string/ImplStrStr.java
@@ -5,11 +5,30 @@ import leetcode.util.ResultCheck;
 /**
  * Created by 曹云 on 2020/8/6.
  * 28. Implement strStr()
+ * https://leetcode.com/problems/implement-strstr/
  */
 public class ImplStrStr {
 
 	public int strStr(String haystack, String needle) {
-		return 0;
+		if (haystack == null || needle == null) return -1;
+		if (haystack.equals(needle) || needle.length() == 0) return 0;
+		if (haystack.length() < needle.length()) return -1;
+		for (int i=0;i<haystack.length() + 1 - needle.length();i++) {
+			if (haystack.charAt(i) == needle.charAt(0)) {
+				boolean pass = true;
+				for (int j = 1;j<needle.length();j++) {
+					if (haystack.charAt(i+j) != needle.charAt(j)) {
+						pass = false;
+						break;
+					}
+				}
+				if (pass) {
+					return i;
+				}
+			}
+		}
+
+		return -1;
 	}
 
 	public static void main(String[] args){
