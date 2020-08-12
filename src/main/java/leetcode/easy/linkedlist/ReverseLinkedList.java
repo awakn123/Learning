401790e54abbf42a798e67f1afa0861c6a6e31fe
awakn123 <awakn123@gmail.com>
2020-08-12 12:22:41 +0800

    #leetcode# reverselinkedlist

--- src/main/java/leetcode/easy/linkedlist/ReverseLinkedList.java
+++ src/main/java/leetcode/easy/linkedlist/ReverseLinkedList.java
@@ -11,14 +11,30 @@ import leetcode.util.ResultCheck;
  */
 public class ReverseLinkedList {
 	public ListNode reverseList(ListNode head) {
-		return null;
+		ListNode result = null;
+		while (head != null) {
+			ListNode tmp = head;
+			head = head.next;
+			tmp.next = result;
+			result = tmp;
+		}
+		return result;
+	}
+
+	public ListNode reverseList2(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+		ListNode parent = reverseList2(head.next);
+		head.next.next = head;
+		head.next = null;
+		return parent;
 	}
 
 	public static void main(String[] args){
 		ReverseLinkedList main = new ReverseLinkedList();
 		ListNode headI = ListNodeUtil.createListNode(new int[]{1,2,3,4,5});
 		ListNode answerI = ListNodeUtil.createListNode(new int[]{5,4,3,2,1});
-		ListNode resultI = main.reverseList(headI);
+		ListNode resultI = main.reverseList2(headI);
 		ResultCheck.check(resultI, answerI);
 	}
 }
