8e4d726bd2d11c5a9ef75f9568672d7a32c0794f
awakn123 <awakn123@gmail.com>
2020-08-12 14:30:15 +0800

    #leetcode# panlindrome & linked list cycle.

--- src/main/java/leetcode/easy/linkedlist/LinkedListCycle.java
+++ src/main/java/leetcode/easy/linkedlist/LinkedListCycle.java
@@ -4,6 +4,9 @@ import leetcode.ListNode;
 import leetcode.util.ListNodeUtil;
 import leetcode.util.ResultCheck;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by 曹云 on 2020/8/11.
  * 141. Linked List Cycle
@@ -11,7 +14,24 @@ import leetcode.util.ResultCheck;
  */
 public class LinkedListCycle {
 	public boolean hasCycle(ListNode head) {
+		Set<Integer> vals = new HashSet();
+		while (head != null) {
+			if (vals.contains(head.val))
+				return true;
+			vals.add(head.val);
+			head = head.next;
+		}
 		return false;
+	}
+	public boolean hasCycle2(ListNode head) {
+		if (head == null || head.next == null) return false;
+		ListNode fast = head.next, slow = head;
+		while (slow != fast) {
+			if (fast == null || fast.next == null) return false;
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		return true;
 	}
 
 	public static void main(String[] args){
@@ -19,16 +39,16 @@ public class LinkedListCycle {
 		ListNode headI = ListNodeUtil.createListNode(new int[]{3,2,0,-4});
 		ListNodeUtil.connectToCycle(headI, 1);
 		boolean answerI = true;
-		boolean resultI = main.hasCycle(headI);
+		boolean resultI = main.hasCycle2(headI);
 		ResultCheck.check(resultI, answerI);
 		ListNode headII = ListNodeUtil.createListNode(new int[]{1, 2});
 		ListNodeUtil.connectToCycle(headII, 0);
 		boolean answerII = true;
-		boolean resultII = main.hasCycle(headII);
+		boolean resultII = main.hasCycle2(headII);
 		ResultCheck.check(resultII, answerII);
 		ListNode headIII = ListNodeUtil.createListNode(new int[]{1});
 		boolean answerIII = false;
-		boolean resultIII = main.hasCycle(headII);
+		boolean resultIII = main.hasCycle2(headIII);
 		ResultCheck.check(resultIII, answerIII);
 	}
 }
--- src/main/java/leetcode/easy/linkedlist/PalindromeLinkedList.java
+++ src/main/java/leetcode/easy/linkedlist/PalindromeLinkedList.java
@@ -4,24 +4,102 @@ import leetcode.ListNode;
 import leetcode.util.ListNodeUtil;
 import leetcode.util.ResultCheck;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by 曹云 on 2020/8/11.
  * 234. Palindrome Linked List
  * https://leetcode.com/problems/palindrome-linked-list/
  */
 public class PalindromeLinkedList {
+	/**
+	 * two pointers.
+	 * @param head
+	 * @return
+	 */
 	public boolean isPalindrome(ListNode head) {
-		return false;
+		List<Integer> list = new ArrayList();
+		while (head != null) {
+			list.add(head.val);
+			head = head.next;
+		}
+		int left = 0, right = list.size() - 1;
+		while (left < right) {
+			if (list.get(left) != list.get(right))
+				return false;
+			left++;
+			right--;
+		}
+		return true;
 	}
+
+	/**
+	 * recurse
+	 * @param head
+	 * @return
+	 */
+	public boolean isPalindrome2(ListNode head) {
+		if (head == null) return true;
+		return recurse(head, head) != null;
+	}
+
+	private ListNode recurse(ListNode node, ListNode head){
+		if (node.next != null)
+			head = recurse(node.next, head);
+		if (head == null || node.val != head.val) return null;
+		return head.next == null ? head : head.next;
+	}
+
+	/**
+	 * O(1) extra space.
+	 * use many loop to reduce extra space. time to space.
+	 * @param head
+	 * @return
+	 */
+	public boolean isPalindrome3(ListNode head) {
+//		if (head == null) return true;
+		ListNode halfNode = this.getHalfList(head);
+		ListNode rehalfNode = this.reverseListNode(halfNode);
+		ListNode re = rehalfNode, h = head;
+		while (re != null) {
+			if (re.val != h.val)
+				return false;
+			re = re.next;
+			h = h.next;
+		}
+		this.reverseListNode(rehalfNode);
+		return true;
+	}
+
+	private ListNode getHalfList(ListNode head) {
+		ListNode slow = head, fast = head;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+	private ListNode reverseListNode(ListNode head) {
+		ListNode result = null;
+		while (head != null) {
+			ListNode tmp = head;
+			head = head.next;
+			tmp.next = result;
+			result = tmp;
+		}
+		return result;
+	}
+
 
 	public static void main(String[] args){
 		PalindromeLinkedList main = new PalindromeLinkedList();
 		ListNode headI = ListNodeUtil.createListNode(new int[]{1,2});
-		boolean resultI = main.isPalindrome(headI);
+		boolean resultI = main.isPalindrome3(headI);
 		boolean answerI = false;
 		ResultCheck.check(resultI, answerI);
 		ListNode headII = ListNodeUtil.createListNode(new int[]{1,2,2,1});
-		boolean resultII = main.isPalindrome(headI);
+		boolean resultII = main.isPalindrome3(headII);
 		boolean answerII = true;
 		ResultCheck.check(resultII, answerII);
 	}
