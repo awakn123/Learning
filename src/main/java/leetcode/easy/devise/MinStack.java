03ad490fa38b6441b3e28460c6be34a8aa8d3a8c
awakn123 <awakn123@gmail.com>
2020-08-16 17:52:17 +0800

    #leetcode# test prepare.

--- src/main/java/leetcode/easy/devise/MinStack.java
+++ src/main/java/leetcode/easy/devise/MinStack.java
@@ -1,5 +1,7 @@
 package leetcode.easy.devise;
 
+import leetcode.util.ResultCheck;
+
 /**
  * Created by 曹云 on 2020/8/16.
  */
@@ -23,5 +25,18 @@ public class MinStack {
 
 	public int getMin() {
 		return 0;
+	}
+
+	public static void main(String[] args){
+
+		MinStack minStack = new MinStack();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+		ResultCheck.check(minStack.getMin(), -3);// --> 返回 -3.
+		minStack.pop();
+		ResultCheck.check(minStack.top(), 0);// --> 返回 0.
+		ResultCheck.check(minStack.getMin(), -2);//   --> 返回 -2.
+
 	}
 }
