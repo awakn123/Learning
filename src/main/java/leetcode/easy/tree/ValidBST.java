c04c4473be41472e4cd738c90fda23d62ee30f6d
awakn123 <awakn123@gmail.com>
2020-08-16 11:27:00 +0800

    #leetcode# valid bst.

--- src/main/java/leetcode/easy/tree/ValidBST.java
+++ src/main/java/leetcode/easy/tree/ValidBST.java
@@ -4,14 +4,40 @@ import leetcode.util.ResultCheck;
 import leetcode.util.TreeNode;
 import leetcode.util.TreeNodeUtil;
 
+import java.util.*;
+
 /**
  * Created by 曹云 on 2020/8/12.
  * 98. Validate Binary Search Tree
  * https://leetcode.com/problems/validate-binary-search-tree/
  */
 public class ValidBST {
+	public boolean isValidBST2(TreeNode root) {
+		return isValidBST(root, null, null);
+	}
+
+	public boolean isValidBST(TreeNode root, Integer min, Integer max) {
+		if (root == null) return true;
+		if (min != null && root.val <= min) return false;
+		if (max != null && root.val >= max) return false;
+		return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+	}
+
 	public boolean isValidBST(TreeNode root) {
-		return false;
+		if (root == null) return true;
+		Stack<TreeNode> stack = new Stack<>();
+		Integer val = null;
+		do {
+			while (root != null) {
+				stack.push(root);
+				root = root.left;
+			}
+			root = stack.pop();
+			if (val != null && val >= root.val) return false;
+			val = root.val;
+			root = root.right;
+		} while (!stack.isEmpty() || root != null);
+		return true;
 	}
 
 	public static void main(String[] args){
@@ -24,9 +50,17 @@ public class ValidBST {
 		boolean resultII = main.isValidBST(rootII);
 		boolean answerII = false;
 		ResultCheck.check(resultII, answerII);
-		TreeNode rootIII = TreeNodeUtil.createTreeNode(new Integer[]{5,1,7,null,null,3,8});
+		TreeNode rootIII = TreeNodeUtil.createTreeNode(new Integer[]{5,1,7,null,null,6,8});
 		boolean resultIII = main.isValidBST(rootIII);
 		boolean answerIII = true;
 		ResultCheck.check(resultIII, answerIII);
+		TreeNode rootIV = TreeNodeUtil.createTreeNode(new Integer[]{3,1,5,0,2,4,6,null,null,null,3});
+		boolean resultIV = main.isValidBST(rootIV);
+		boolean answerIV = false;
+		ResultCheck.check(resultIV, answerIV);
+		TreeNode rootV = TreeNodeUtil.createTreeNode(new Integer[]{-2147483648,null,2147483647});
+		boolean resultV = main.isValidBST(rootV);
+		boolean answerV = true;
+		ResultCheck.check(resultV, answerV);
 	}
 }
