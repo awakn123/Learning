f4ffbf2483b0a8dc9f05b37c22a6a01840b8747c
awakn123 <awakn123@gmail.com>
2020-08-16 17:07:37 +0800

    #leetcode# devise class prepare.

---
+++ src/main/java/leetcode/easy/devise/MinStack.java
@@ -0,0 +1,27 @@
+package leetcode.easy.devise;
+
+/**
+ * Created by 曹云 on 2020/8/16.
+ */
+public class MinStack {
+
+	public MinStack() {
+
+	}
+
+	public void push(int x) {
+
+	}
+
+	public void pop() {
+
+	}
+
+	public int top() {
+		return 0;
+	}
+
+	public int getMin() {
+		return 0;
+	}
+}
---
+++ src/main/java/leetcode/easy/devise/ShuffleArray.java
@@ -0,0 +1,23 @@
+package leetcode.easy.devise;
+
+/**
+ * Created by 曹云 on 2020/8/16.
+ * 384. Shuffle an Array
+ * https://leetcode.com/problems/shuffle-an-array/
+ */
+public class ShuffleArray {
+
+	public ShuffleArray(int[] nums) {
+
+	}
+
+	/** Resets the array to its original configuration and return it. */
+	public int[] reset() {
+		return null;
+	}
+
+	/** Returns a random shuffling of the array. */
+	public int[] shuffle() {
+		return null;
+	}
+}
