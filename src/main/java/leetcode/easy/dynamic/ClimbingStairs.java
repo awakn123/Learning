4a11f84a244e9804ec0996463546b050fa72ddd4
awakn123 <awakn123@gmail.com>
2020-08-17 08:58:22 +0800

    #leetcode# climb stairs.

--- src/main/java/leetcode/easy/dynamic/ClimbingStairs.java
+++ src/main/java/leetcode/easy/dynamic/ClimbingStairs.java
@@ -8,13 +8,42 @@ import leetcode.util.ResultCheck;
  * https://leetcode.com/problems/climbing-stairs/
  */
 public class ClimbingStairs {
+	public int climbStairs2(int n) {
+		int twoNum = n >> 1, result = 0;
+		for (int i=0; i<twoNum+1; i++) {
+			result += combination(n-i, i);
+		}
+		return result;
+	}
+
+	private int combination(int n, int r) {
+		if (n < r) return 1;
+		Long denominator, numerator, result = new Long(1);
+		r = Math.min(n-r, r);
+		for (int i=1; i<=r; i++) {
+			numerator = new Long(n - r + i);
+			denominator = new Long(i);
+			result = result * numerator/ denominator;
+		}
+		return result.intValue();
+	}
+
+
 	public int climbStairs(int n) {
-		return 0;
+		int p=0, q=0, result = 1;
+		for (int i=0;i<n;i++) {
+			p = q;
+			q = result;
+			result = p+q;
+		}
+		return result;
 	}
 
 	public static void main(String[] args){
 		ClimbingStairs main = new ClimbingStairs();
 		ResultCheck.check(main.climbStairs(2), 2);
 		ResultCheck.check(main.climbStairs(3), 3);
+		ResultCheck.check(main.climbStairs(35), 14930352);
+		ResultCheck.check(main.climbStairs(44), 1134903170);
 	}
 }
