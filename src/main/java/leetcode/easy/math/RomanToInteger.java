e9ab8b140a30b6067bb06ecc5fd10ef2f6a46f0b
awakn123 <awakn123@gmail.com>
2020-08-17 17:51:13 +0800

    #leetcode# roman to Integer.

--- src/main/java/leetcode/easy/math/RomanToInteger.java
+++ src/main/java/leetcode/easy/math/RomanToInteger.java
@@ -2,6 +2,9 @@ package leetcode.easy.math;
 
 import leetcode.util.ResultCheck;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by 曹云 on 2020/8/16.
  * 13. Roman to Integer
@@ -10,7 +13,38 @@ import leetcode.util.ResultCheck;
 public class RomanToInteger {
 
 	public int romanToInt(String s) {
-		return 0;
+		int result = 0;
+		for (int i = s.length() - 1; i>=0; i--) {
+			Character c = s.charAt(i), prevc = i > 0 ? s.charAt(i-1) : null;
+			result += getValue(c);
+			if (prevc != null) {
+				if (prevc == 'I' && (c == 'V' || c == 'X')) {
+					result--;
+					i--;
+				} else if (prevc == 'X' && (c == 'L' || c == 'C')) {
+					result -= 10;
+					i--;
+				} else if (prevc == 'C' && (c == 'D' || c =='M')) {
+					result -= 100;
+					i--;
+				}
+			}
+		}
+		return result;
+	}
+
+
+	private int getValue(char ch) {
+		switch(ch) {
+			case 'I': return 1;
+			case 'V': return 5;
+			case 'X': return 10;
+			case 'L': return 50;
+			case 'C': return 100;
+			case 'D': return 500;
+			case 'M': return 1000;
+			default: return 0;
+		}
 	}
 
 	public static void main(String[] args){
