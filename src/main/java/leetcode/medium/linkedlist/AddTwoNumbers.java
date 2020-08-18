013f42520316c75e05ff7ebf83b1901225f76a8f
awakn123 <awakn123@gmail.com>
2020-08-18 15:16:21 +0800

    #leetcode# medium linkedlist. class prepare.

---
+++ src/main/java/leetcode/medium/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package leetcode.medium.linkedlist;
+
+import leetcode.ListNode;
+
+/**
+ * Created by 曹云 on 2020/8/18.
+ * 2. Add Two Numbers
+ * https://leetcode-cn.com/problems/add-two-numbers/solution/
+ */
+public class AddTwoNumbers {
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		return null;
+	}
+}
---
+++ src/main/java/leetcode/medium/linkedlist/OddEvenLinkedList.java
@@ -0,0 +1,14 @@
+package leetcode.medium.linkedlist;
+
+import leetcode.ListNode;
+
+/**
+ * Created by 曹云 on 2020/8/18.
+ * 328. Odd even linked list.
+ * https://leetcode-cn.com/problems/odd-even-linked-list/solution/
+ */
+public class OddEvenLinkedList {
+	public ListNode oddEvenList(ListNode head) {
+		return null;
+	}
+}
