a903aeb448fdf4f9be37771b673b87c8bebb9c06
awakn123 <awakn123@gmail.com>
2020-08-18 15:16:13 +0800

    #leetcode# medium linkedlist. class prepare.

---
+++ src/main/java/leetcode/medium/linkedlist/IntersectionTwoLinkedList.java
@@ -0,0 +1,14 @@
+package leetcode.medium.linkedlist;
+
+import leetcode.ListNode;
+
+/**
+ * Created by 曹云 on 2020/8/18.
+ * 160. 相交链表 Intersection of Two Linked Lists.
+ */
+public class IntersectionTwoLinkedList {
+
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		return null;
+	}
+}
