ee448fd3fb1811f49c1f024a8d6738a72640c6b5
awakn123 <awakn123@gmail.com>
2020-08-18 15:27:19 +0800

    #leetcode# medium tree and graph. class prepare.

---
+++ src/main/java/leetcode/medium/treeandgraph/NumberofIsalnds.java
@@ -0,0 +1,13 @@
+package leetcode.medium.treeandgraph;
+
+/**
+ * Created by 曹云 on 2020/8/18.
+ * 200. 岛屿数量
+ * https://leetcode-cn.com/problems/number-of-islands/solution/
+ */
+public class NumberofIsalnds {
+
+	public int numIslands(char[][] grid) {
+		return 0;
+	}
+}
