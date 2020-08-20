230be6535ba5cbe2a677a08a2c2f979561d3d684
awakn123 <awakn123@gmail.com>
2020-08-20 16:20:22 +0800

    #leetcode# medium GroupAnagrams.

--- src/main/java/leetcode/medium/arrayandstr/GroupAnagrams.java
+++ src/main/java/leetcode/medium/arrayandstr/GroupAnagrams.java
@@ -3,7 +3,8 @@ package leetcode.medium.arrayandstr;
 import com.google.common.collect.Lists;
 import leetcode.util.ResultCheck;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by 曹云 on 2020/8/18.
@@ -11,8 +12,41 @@ import java.util.List;
  * https://leetcode-cn.com/problems/group-anagrams/solution/
  */
 public class GroupAnagrams {
+	public List<List<String>> groupAnagrams2(String[] strs) {
+		Map<String, List<String>> map = new HashMap<>();
+		for (int i=0; i<strs.length; i++) {
+			char[] chars = strs[i].toCharArray();
+			Arrays.sort(chars);
+			String str = String.valueOf(chars);
+			List<String> l = map.getOrDefault(str, new ArrayList<>());
+			l.add(strs[i]);
+			map.put(str, l);
+		}
+		return new ArrayList(map.values());
+	}
+
+	/**
+	 * 两种方式没有本质上的区别，都是排序方式。第二种只是空间换时间，而且换的还挺笨的，还是不要用的好，理解思路。
+	 * @param strs
+	 * @return
+	 */
 	public List<List<String>> groupAnagrams(String[] strs) {
-		return null;
+		Map<String, List<String>> map = new HashMap<>();
+		for (int i=0;i<strs.length;i++) {
+			int[] count = new int[26];
+			for (int j=0;j<strs[i].length(); j++) {
+				count[strs[i].charAt(j) - 'a']++;
+			}
+			StringBuilder out = new StringBuilder();
+			for (int j=0; j<count.length; j++) {
+				out.append("#").append(count[j]);
+			}
+			String str = out.toString();
+			List<String> l = map.getOrDefault(str, new ArrayList<>());
+			l.add(strs[i]);
+			map.put(str, l);
+		}
+		return new ArrayList(map.values());
 	}
 
 	public static void main(String[] args){
@@ -23,5 +57,10 @@ public class GroupAnagrams {
 				Lists.newArrayList("bat")
 		);
 		ResultCheck.checkTwoDimension(main.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}), answer);
+		List<List<String>> answerII = Lists.newArrayList(
+				Lists.newArrayList("boo"),
+				Lists.newArrayList("bob")
+		);
+		ResultCheck.checkTwoDimension(main.groupAnagrams(new String[]{"boo", "bob"}), answerII);
 	}
 }
