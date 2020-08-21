5ea483aa4f9ab60c78dc355e0a60ed3e3b08abfc
awakn123 <awakn123@gmail.com>
2020-08-21 11:47:47 +0800

    #leetcode# medium add two numbers

--- src/main/java/leetcode/medium/linkedlist/AddTwoNumbers.java
+++ src/main/java/leetcode/medium/linkedlist/AddTwoNumbers.java
@@ -12,7 +12,22 @@ import leetcode.util.ResultCheck;
 public class AddTwoNumbers {
 
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		return null;
+		ListNode dummy = new ListNode(), node = dummy;
+		int plus = 0;
+		while(l1 != null || l2 != null) {
+			int val = (l1 != null ? l1.val : 0)
+					+ (l2 != null ? l2.val : 0)
+					+ plus;
+			plus = val/10;
+			val %= 10;
+			node.next = new ListNode(val);
+			node = node.next;
+			l1 = l1 == null ? null : l1.next;
+			l2 = l2 == null ? null : l2.next;
+		}
+		if (plus > 0)
+			node.next = new ListNode(plus);
+		return dummy.next;
 	}
 
 	public static void main(String[] args){
