84c1a60a17ef3a89d75c54bdf735c16e6f5d8fe9
awakn123 <awakn123@gmail.com>
2020-08-23 07:10:45 +0800

    #leetcode# medium NumberOfIslands

--- src/main/java/leetcode/medium/treeandgraph/NumberofIslands.java
+++ src/main/java/leetcode/medium/treeandgraph/NumberofIslands.java
@@ -7,14 +7,33 @@ import leetcode.util.ResultCheck;
  * 200. 岛屿数量
  * https://leetcode-cn.com/problems/number-of-islands/solution/
  */
-public class NumberofIsalnds {
+public class NumberofIslands {
 
 	public int numIslands(char[][] grid) {
-		return 0;
+		int num = 0;
+		for (int i=0; i<grid.length; i++) {
+			for (int j=0; j<grid[i].length; j++) {
+				if (grid[i][j] == '0')
+					continue;
+				num++;
+				dfs(grid, i, j);
+			}
+		}
+		return num;
+	}
+
+	private void dfs (char[][] grid, int i, int j) {
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0')
+			return;
+		grid[i][j] = '0';
+		dfs(grid, i+1, j);
+		dfs(grid, i-1, j);
+		dfs(grid, i, j+1);
+		dfs(grid, i, j-1);
 	}
 
 	public static void main(String[] args){
-		NumberofIsalnds main = new NumberofIsalnds();
+		NumberofIslands main = new NumberofIslands();
 		char[][] grid = new char[][]{
 				new char[]{'1','1','1','1','0'},
 				new char[]{'1','1','0','1','0'},
