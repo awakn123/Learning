d2c7f0ee8cfcd7f61b1e076fab13e5d8d7443d8c
awakn123 <awakn123@gmail.com>
2020-08-25 15:24:46 +0800

    #leetcode# medium sort colors

--- src/main/java/leetcode/medium/sortandsearch/SortColors.java
+++ src/main/java/leetcode/medium/sortandsearch/SortColors.java
@@ -9,7 +9,23 @@ import leetcode.util.ResultCheck;
  */
 public class SortColors {
 	public void sortColors(int[] nums) {
+		int left = 0, right = nums.length - 1;
+		for (int i=0; i<=right; i++) {
+			if (nums[i] == 2) {
+				swap(nums, i, right);
+				right--;
+				i--;
+			} else if (nums[i] == 0) {
+				swap(nums, i ,left);
+				left++;
+			}
+		}
+	}
 
+	private void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
 	}
 
 	public static void main(String[] args){
@@ -17,5 +33,8 @@ public class SortColors {
 		int[] nums = new int[]{2,0,2,1,1,0};
 		main.sortColors(nums);
 		ResultCheck.check(nums, new int[]{0,0,1,1,2,2});
+		int[] numsII = new int[]{2,0,1};
+		main.sortColors(numsII);
+		ResultCheck.check(numsII, new int[]{0,1,2});
 	}
 }
