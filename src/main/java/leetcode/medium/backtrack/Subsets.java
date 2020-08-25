ea9fd0f1ecaec725e2782dc022a54018d43b9c0b
awakn123 <awakn123@gmail.com>
2020-08-25 13:14:53 +0800

    #leetcode# medium subset.

--- src/main/java/leetcode/medium/backtrack/Subsets.java
+++ src/main/java/leetcode/medium/backtrack/Subsets.java
@@ -3,6 +3,7 @@ package leetcode.medium.backtrack;
 import com.google.common.collect.Lists;
 import leetcode.util.ResultCheck;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,21 +13,55 @@ import java.util.List;
  */
 public class Subsets {
 
-	public List<List<Integer>> subsets(int[] nums) {
-		return null;
+	public List<List<Integer>> subsets1(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		for (int len=0; len<=nums.length; len++) {
+			backtrack(nums, len, new ArrayList<>(), 0, result);
+		}
+		return result;
 	}
+
+	private void backtrack(int[] nums, int len, List<Integer> out, int first, List<List<Integer>> result) {
+		if (out.size() == len) {
+			result.add(new ArrayList<>(out));
+			return;
+		}
+		for (int i=first; i<nums.length; i++) {
+			out.add(nums[i]);
+			backtrack(nums, len, out, i + 1, result);
+			out.remove(out.size() - 1);
+		}
+	}
+
+	public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> result = new ArrayList<>();
+		int n = nums.length;
+		for (int i=0;i<(int)Math.pow(2, n); i++) {
+			List<Integer> out = new ArrayList<>();
+			int combination = i;
+			for (int j=0; j<n; j++) {
+				if (combination%2 == 1) {
+					out.add(nums[j]);
+				}
+				combination >>= 1;
+			}
+			result.add(out);
+		}
+		return result;
+	}
+
 
 	public static void main(String[] args){
 		Subsets main = new Subsets();
 		ResultCheck.checkTwoDimension(main.subsets(new int[]{1,2,3}), Lists.newArrayList(
-				Lists.newArrayList(3),
+				Lists.newArrayList(),
 				Lists.newArrayList(1),
 				Lists.newArrayList(2),
-				Lists.newArrayList(1,2,3),
+				Lists.newArrayList(3),
+				Lists.newArrayList(1,2),
 				Lists.newArrayList(1,3),
 				Lists.newArrayList(2,3),
-				Lists.newArrayList(1,2),
-				Lists.newArrayList()
+				Lists.newArrayList(1,2,3)
 		));
 	}
 }
