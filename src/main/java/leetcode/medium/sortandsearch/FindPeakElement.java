5d66e388c465862c3dd7992ec60ecf3dfa8a07e9
awakn123 <awakn123@gmail.com>
2020-08-25 18:16:35 +0800

    #leetcode# medium FindPeakElement

--- src/main/java/leetcode/medium/sortandsearch/FindPeakElement.java
+++ src/main/java/leetcode/medium/sortandsearch/FindPeakElement.java
@@ -9,13 +9,31 @@ import leetcode.util.ResultCheck;
  */
 public class FindPeakElement {
 
+	public int findPeakElement1(int[] nums) {
+		for (int i=1;i<nums.length;i++) {
+			if (nums[i] < nums[i - 1])
+				return i - 1;
+		}
+		return nums.length - 1;
+	}
 	public int findPeakElement(int[] nums) {
-		return 0;
+		return findPeakElement(nums, 0, nums.length - 1);
+	}
+	public int findPeakElement(int[] nums, int left, int right) {
+		int len = right - left + 1;
+		int mid = (len >> 1) + left;
+		if (mid + 1 >= nums.length || nums[mid] > nums[mid + 1]) {
+			if (mid - 1 < 0 || nums[mid] > nums[mid - 1])
+				return mid;
+			return findPeakElement(nums, left, mid - 1);
+		}
+		return findPeakElement(nums, mid + 1, right);
 	}
 
 	public static void main(String[] args){
 		FindPeakElement main = new FindPeakElement();
 		ResultCheck.check(main.findPeakElement(new int[]{1,2,3,1}), 2);
+		ResultCheck.check(main.findPeakElement(new int[]{1,2,3}), 2);
 		ResultCheck.check(main.findPeakElement(new int[]{1,2,1,3,5,6,4}), 1);// or 5.
 	}
 }
