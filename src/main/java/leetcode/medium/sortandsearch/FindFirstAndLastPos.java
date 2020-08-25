676e291deeb96deaf85f8fd26210443a4d3d8082
awakn123 <awakn123@gmail.com>
2020-08-25 23:22:36 +0800

    #leetcode# medium FindFirstAndLastPosition

--- src/main/java/leetcode/medium/sortandsearch/FindFirstAndLastPos.java
+++ src/main/java/leetcode/medium/sortandsearch/FindFirstAndLastPos.java
@@ -10,7 +10,30 @@ import leetcode.util.ResultCheck;
 public class FindFirstAndLastPos {
 
 	public int[] searchRange(int[] nums, int target) {
-		return nums;
+		int[] res = new int[]{-1, -1};
+		int left = 0, right = nums.length, targetIdx = -1;
+		while(left < right) {
+			int mid = left + ((right - left) >> 1);
+			if (nums[mid] > target) {
+				right = mid;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				targetIdx = mid;
+				break;
+			}
+		}
+		if (targetIdx == -1)
+			return res;
+		res[0] = targetIdx;
+		while(res[0]>=0 && nums[res[0]] == nums[res[0] - 1]) {
+			res[0]--;
+		}
+		res[1] = targetIdx;
+		while(res[1]<nums.length && nums[res[1]] == nums[res[1] + 1]) {
+			res[1]++;
+		}
+		return res;
 	}
 
 	public static void main(String[] args){
