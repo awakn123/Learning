f158d6410092ff75f859588a32a63e219634d028
awakn123 <awakn123@gmail.com>
2020-08-25 16:15:10 +0800

    #leetcode# medium top K frequent elements

--- src/main/java/leetcode/medium/sortandsearch/TopKFrequentElements.java
+++ src/main/java/leetcode/medium/sortandsearch/TopKFrequentElements.java
@@ -2,6 +2,8 @@ package leetcode.medium.sortandsearch;
 
 import leetcode.util.ResultCheck;
 
+import java.util.*;
+
 /**
  * Created by 曹云 on 2020/8/25.
  * 347. 前 K 个高频元素
@@ -9,9 +11,68 @@ import leetcode.util.ResultCheck;
  */
 public class TopKFrequentElements {
 
-	public int[] topKFrequent(int[] nums, int k) {
-		return nums;
+	public int[] topKFrequent1(int[] nums, int k) {
+		Map<Integer, Integer> numMap = new HashMap<>();
+		for (int i=0; i<nums.length; i++) {
+			numMap.put(nums[i], numMap.getOrDefault(nums[i], 0) + 1);
+		}
+		Iterator<Map.Entry<Integer, Integer>> iter = numMap.entrySet().iterator();
+		int[][] idx = new int[nums.length + 1][];
+		while(iter.hasNext()) {
+			Map.Entry<Integer, Integer> e = iter.next();
+			if (idx[e.getValue()] == null) {
+				idx[e.getValue()] = new int[]{e.getKey()};
+			} else {
+				idx[e.getValue()] = Arrays.copyOf(idx[e.getValue()], idx[e.getValue()].length + 1);
+				idx[e.getValue()][idx[e.getValue()].length - 1] = e.getKey();
+			}
+
+		}
+
+		int j = 0;
+		int[] result = new int[k];
+		for (int i=idx.length - 1;i>0;i--) {
+			if (j == k)
+				break;
+			if (idx[i] != null) {
+				for (int p=0;p<idx[i].length;p++) {
+					if (j == k)
+						break;
+					result[j] = idx[i][p];
+					j++;
+				}
+			}
+		}
+		return result;
 	}
+	public int[] topKFrequent(int[] nums, int k) {
+		Map<Integer, Integer> numMap = new HashMap<>();
+		for (int i=0; i<nums.length; i++) {
+			numMap.put(nums[i], numMap.getOrDefault(nums[i], 0) + 1);
+		}
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return numMap.get(o1) - numMap.get(o2);
+			}
+		});
+		for (Integer num: numMap.keySet()) {
+			if (pq.size() < k) {
+				pq.add(num);
+			} else if (numMap.get(pq.peek()) < numMap.get(num)) {
+				pq.remove();
+				pq.add(num);
+			}
+		}
+		int[] result = new int[k];
+		int j = k - 1;
+		while(!pq.isEmpty()) {
+			result[j] = pq.poll();
+			j--;
+		}
+		return result;
+	}
+
 
 	public static void main(String[] args){
 		TopKFrequentElements main = new TopKFrequentElements();
