7732371bfb4a3fe9755c8ce3e5c37489762c1e22
awakn123 <awakn123@gmail.com>
2020-08-26 10:51:56 +0800

    #leetcode# medium merge intervals

--- src/main/java/leetcode/medium/sortandsearch/MergeIntervals.java
+++ src/main/java/leetcode/medium/sortandsearch/MergeIntervals.java
@@ -2,6 +2,11 @@ package leetcode.medium.sortandsearch;
 
 import leetcode.util.ResultCheck;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by 曹云 on 2020/8/25.
  * 56. 合并区间
@@ -10,7 +15,27 @@ import leetcode.util.ResultCheck;
 public class MergeIntervals {
 
 	public int[][] merge(int[][] intervals) {
-		return intervals;
+		if (intervals.length == 0) return intervals;
+		int[][] result = new int[intervals.length][];
+		int len = 0;
+		Arrays.sort(intervals, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+		int[] merge = intervals[0];
+		for (int i=1; i<intervals.length; i++) {
+			int[] num = intervals[i];
+			if (num[0] > merge[1]) {
+				result[len++] = merge;
+				merge = num;
+				continue;
+			}
+			merge[1] = Math.max(num[1], merge[1]);
+		}
+		result[len++] = merge;
+		return Arrays.copyOf(result, len);
 	}
 
 	public static void main(String[] args){
