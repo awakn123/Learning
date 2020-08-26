3b425991f60e0c546718feca077fe5e793ff419f
awakn123 <awakn123@gmail.com>
2020-08-26 12:02:08 +0800

    #leetcode# medium search in rotated sorted array.

--- src/main/java/leetcode/medium/sortandsearch/SearchInRotatedSortedArray.java
+++ src/main/java/leetcode/medium/sortandsearch/SearchInRotatedSortedArray.java
@@ -10,7 +10,50 @@ import leetcode.util.ResultCheck;
 public class SearchInRotatedSortedArray {
 
 	public int search(int[] nums, int target) {
-		return 0;
+		return search(nums, target, 0, nums.length);
+	}
+
+	private int search1(int[] nums, int target, int left, int right) {
+		if (left >= right)
+			return -1;
+		int mid = (right - left)/2 + left;
+		if (nums[mid] == target)
+			return mid;
+		if (nums[left] == target)
+			return left;
+		if (nums[right - 1] == target)
+			return right - 1;
+
+		boolean bigM = target > nums[mid], bigL = target > nums[left], bigR = target > nums[right - 1];
+		if (bigM && !bigR)
+			return search1(nums, target, mid + 1, right);
+		else if (!bigM && bigL)
+			return search1(nums, target, left, mid);
+		else {
+			int leftIdx = search1(nums, target, left, mid);
+			if (leftIdx == -1)
+				return search1(nums, target, mid + 1, right);
+			return leftIdx;
+		}
+	}
+
+	private int search(int[] nums, int target, int left, int right) {
+		if (left >= right)
+			return -1;
+		int mid = (right - left)/2 + left;
+		if (nums[mid] == target)
+			return mid;
+		if (nums[left] < nums[mid]) {// left part is sorted
+			if (target < nums[mid] && target >= nums[left])
+				return search(nums, target, left, mid);
+			else
+				return search(nums, target, mid + 1, right);
+		} else {// right part is sorted
+			if (target > nums[mid] && target <= nums[right - 1])
+				return search(nums, target, mid + 1, right);
+			else
+				return search(nums, target, left, mid);
+		}
 	}
 
 	public static void main(String[] args){
