6e2c0503f329f60a6f43670df9e361aefb942459
awakn123 <awakn123@gmail.com>
2020-08-27 23:29:52 +0800

    #leetcode# medium design class prepare.

---
+++ src/main/java/leetcode/medium/design/InsertDeleteGetRandomO1.java
@@ -0,0 +1,28 @@
+package leetcode.medium.design;
+
+/**
+ * Created by 曹云 on 2020/8/27.
+ * 380. 常数时间插入、删除和获取随机元素
+ * https://leetcode-cn.com/problems/insert-delete-getrandom-o1/solution/
+ */
+public class InsertDeleteGetRandomO1 {
+	/** Initialize your data structure here. */
+	public InsertDeleteGetRandomO1() {
+
+	}
+
+	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+	public boolean insert(int val) {
+		return false;
+	}
+
+	/** Removes a value from the set. Returns true if the set contained the specified element. */
+	public boolean remove(int val) {
+		return false;
+	}
+
+	/** Get a random element from the set. */
+	public int getRandom() {
+		return 0;
+	}
+}
---
+++ src/main/java/leetcode/medium/design/SerializeAndDeserializeBTree.java
@@ -0,0 +1,20 @@
+package leetcode.medium.design;
+
+import leetcode.util.TreeNode;
+
+/**
+ * Created by 曹云 on 2020/8/27.
+ * 297. 二叉树的序列化与反序列化
+ * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/solution/
+ */
+public class SerializeAndDeserializeBTree {
+	// Encodes a tree to a single string.
+	public String serialize(TreeNode root) {
+		return null;
+	}
+
+	// Decodes your encoded data to tree.
+	public TreeNode deserialize(String data) {
+		return null;
+	}
+}
