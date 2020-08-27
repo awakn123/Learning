ce79492d4a360a23fee3f9476a51dc214ff4bb48
awakn123 <awakn123@gmail.com>
2020-08-27 23:36:51 +0800

    #leetcode# medium design test prepare.

--- src/main/java/leetcode/medium/design/SerializeAndDeserializeBTree.java
+++ src/main/java/leetcode/medium/design/SerializeAndDeserializeBTree.java
@@ -1,6 +1,8 @@
 package leetcode.medium.design;
 
+import leetcode.util.ResultCheck;
 import leetcode.util.TreeNode;
+import leetcode.util.TreeNodeUtil;
 
 /**
  * Created by 曹云 on 2020/8/27.
@@ -16,5 +18,13 @@ public class SerializeAndDeserializeBTree {
 	// Decodes your encoded data to tree.
 	public TreeNode deserialize(String data) {
 		return null;
+	}
+
+	public static void main(String[] args){
+		SerializeAndDeserializeBTree main = new SerializeAndDeserializeBTree();
+		TreeNode node = TreeNodeUtil.createTreeNode(new Integer[]{1,2,3, null, null, 4, 5});
+		String str = main.serialize(node);
+		ResultCheck.check(str,"[1,2,3,null,null,4,5]");
+		ResultCheck.check(main.deserialize(str), node);
 	}
 }
