16c2f28290d9320a0e94fe22fcf61e2e868cf162
awakn123 <awakn123@gmail.com>
2020-08-28 21:51:30 +0800

    #leetcode# medium Powerxn

--- src/main/java/leetcode/medium/math/Powxn.java
+++ src/main/java/leetcode/medium/math/Powxn.java
@@ -9,7 +9,19 @@ import leetcode.util.ResultCheck;
  */
 public class Powxn {
 	public double myPow(double x, int n) {
-		return 0.0;
+		if (n == 0 || x == 1) return 1;
+		long N = n;
+		if (N < 0)
+			N = -N;
+		double r = 1;
+		while (N > 1) {
+			if (N%2 == 1)
+				r *= x;
+			x = x * x;
+			N = N/2;
+		}
+		r *= x;
+		return n > 0 ? r : 1/r;
 	}
 
 	public static void main(String[] args){
@@ -17,5 +29,6 @@ public class Powxn {
 		ResultCheck.check(main.myPow(2, 10), 1024);
 		ResultCheck.check(main.myPow(2.1, 3), 9.261);
 		ResultCheck.check(main.myPow(2, -2), 0.25);
+		ResultCheck.check(main.myPow(2, -2147483648), 0);
 	}
 }
