1e43991e1684233e2a18d763cb6bd638431a2283
awakn123 <awakn123@gmail.com>
2020-08-28 16:36:45 +0800

    #leetcode# medium random and serialize

--- src/main/java/leetcode/medium/design/InsertDeleteGetRandomO1.java
+++ src/main/java/leetcode/medium/design/InsertDeleteGetRandomO1.java
@@ -1,28 +1,59 @@
 package leetcode.medium.design;
 
+import java.util.*;
+
 /**
  * Created by 曹云 on 2020/8/27.
  * 380. 常数时间插入、删除和获取随机元素
  * https://leetcode-cn.com/problems/insert-delete-getrandom-o1/solution/
  */
 public class InsertDeleteGetRandomO1 {
+	Map<Integer, Integer> map;
+	List<Integer> list;
+	Random random;
+
 	/** Initialize your data structure here. */
 	public InsertDeleteGetRandomO1() {
-
+		map = new HashMap<>();
+		list = new ArrayList<>();
+		random = new Random();
 	}
 
 	/** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
 	public boolean insert(int val) {
-		return false;
+		if (map.containsKey(val))
+			return false;
+		list.add(val);
+		map.put(val, list.size() - 1);
+		return true;
 	}
 
 	/** Removes a value from the set. Returns true if the set contained the specified element. */
 	public boolean remove(int val) {
-		return false;
+		if (!map.containsKey(val))
+			return false;
+		Integer idx = map.get(val), lastNum = list.get(list.size() - 1);
+		list.set(idx, lastNum);
+		map.put(lastNum, idx);
+		list.remove(list.size() - 1);
+		map.remove(val);
+		return true;
 	}
 
 	/** Get a random element from the set. */
 	public int getRandom() {
-		return 0;
+		int idx = random.nextInt(list.size());
+		return list.get(idx);
+	}
+
+	public static void main(String[] args){
+		InsertDeleteGetRandomO1 main = new InsertDeleteGetRandomO1();
+		System.out.printf("insert 1: %b\n", main.insert(1));
+		System.out.printf("remove 2: %b\n", main.remove(2));
+		System.out.printf("insert 2: %b\n", main.insert(2));
+		System.out.printf("random  : %d\n", main.getRandom());
+		System.out.printf("remove 1: %b\n", main.remove(1));
+		System.out.printf("insert 2: %b\n", main.insert(2));
+		System.out.printf("random  : %d\n", main.getRandom());
 	}
 }
--- src/main/java/leetcode/medium/design/SerializeAndDeserializeBTree.java
+++ src/main/java/leetcode/medium/design/SerializeAndDeserializeBTree.java
@@ -3,6 +3,11 @@ package leetcode.medium.design;
 import leetcode.util.ResultCheck;
 import leetcode.util.TreeNode;
 import leetcode.util.TreeNodeUtil;
+import sun.reflect.generics.tree.Tree;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /**
  * Created by 曹云 on 2020/8/27.
@@ -12,12 +17,63 @@ import leetcode.util.TreeNodeUtil;
 public class SerializeAndDeserializeBTree {
 	// Encodes a tree to a single string.
 	public String serialize(TreeNode root) {
-		return null;
+		if (root == null) return "[]";
+		StringBuilder out = new StringBuilder("[");
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.offer(root);
+		while(!queue.isEmpty()) {
+			int size = queue.size();
+			boolean allNull = true;
+			while(size > 0) {
+				TreeNode node = queue.poll();
+				if (node == null) {
+					out.append("null,");
+				} else {
+					out.append(node.val).append(",");
+					queue.offer(node.left);
+					queue.offer(node.right);
+					if (node.left != null || node.right != null)
+						allNull = false;
+				}
+				size--;
+			}
+			if (allNull)
+				queue.clear();
+		}
+		out.deleteCharAt(out.length() - 1).append("]");
+		return out.toString();
 	}
 
 	// Decodes your encoded data to tree.
 	public TreeNode deserialize(String data) {
-		return null;
+		if (data == null || data.length() < 3) return null;
+		String[] numStrs = data.substring(1, data.length() - 1).split(",");
+		if (numStrs.length == 0) return null;
+		TreeNode root = createTreeNode(numStrs[0]);
+		Queue<TreeNode> queue = new LinkedList<TreeNode>(){{add(root);}};
+		int i = 1;
+		while(i < numStrs.length - 1) {
+			TreeNode node = queue.poll();
+			if (node == null) {
+				continue;
+			}
+			node.left = createTreeNode(numStrs[i++]);
+			node.right = createTreeNode(numStrs[i++]);
+			queue.add(node.left);
+			queue.add(node.right);
+		}
+
+		return root;
+	}
+
+	private TreeNode createTreeNode(String numStr) {
+		if ("null".equals(numStr)) return null;
+		try {
+			int num = Integer.parseInt(numStr);
+			return new TreeNode(num);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	public static void main(String[] args){
@@ -26,5 +82,13 @@ public class SerializeAndDeserializeBTree {
 		String str = main.serialize(node);
 		ResultCheck.check(str,"[1,2,3,null,null,4,5]");
 		ResultCheck.check(main.deserialize(str), node);
+		TreeNode nodeII = TreeNodeUtil.createTreeNode(new Integer[]{1,3, null, null, 4});
+		String strII = main.serialize(nodeII);
+		ResultCheck.check(strII,"[1,3,null,null,4]");
+		ResultCheck.check(main.deserialize(strII), nodeII);
+		TreeNode nodeIII = TreeNodeUtil.createTreeNode(new Integer[]{1,2,null,3,null,4,null,5});
+		String strIII = main.serialize(nodeIII);
+		ResultCheck.check(strIII,"[1,2,null,3,null,4,null,5]");
+		ResultCheck.check(main.deserialize(strIII), nodeIII);
 	}
 }
