6febd7a86bc102bad8dcfa47d1de81c7f0ea062e
awakn123 <awakn123@gmail.com>
2020-08-29 17:53:58 +0800

    #leetcode# medium EvaluateReversePolishNotation

--- src/main/java/leetcode/medium/other/EvaluateReversePolishNotation.java
+++ src/main/java/leetcode/medium/other/EvaluateReversePolishNotation.java
@@ -1,5 +1,9 @@
 package leetcode.medium.other;
 
+import leetcode.util.ResultCheck;
+
+import java.util.Stack;
+
 /**
  * Created by 曹云 on 2020/8/29.
  * 150. 逆波兰表达式求值
@@ -8,6 +12,40 @@ package leetcode.medium.other;
 public class EvaluateReversePolishNotation {
 
 	public int evalRPN(String[] tokens) {
-		return 0;
+		Stack<Integer> stack = new Stack<>();
+		for (String s: tokens) {
+			int num;
+			switch (s) {
+				case "+":
+					num = stack.pop() + stack.pop();
+					break;
+				case "-":
+					int first = stack.pop();
+					int second = stack.pop();
+					num = second - first;
+					break;
+				case "*":
+					num = stack.pop() * stack.pop();
+					break;
+				case "/":
+					int divisor = stack.pop();
+					int dividend = stack.pop();
+					num = dividend / divisor;
+					break;
+				default:
+					num = Integer.parseInt(s);
+					break;
+			}
+			stack.push(num);
+		}
+		return stack.pop();
+	}
+
+	public static void main(String[] args){
+		EvaluateReversePolishNotation main = new EvaluateReversePolishNotation();
+		ResultCheck.check(main.evalRPN(new String[]{"2", "1", "+", "3", "*"}), 9);
+		ResultCheck.check(main.evalRPN(new String[]{"4", "13", "5", "/", "+"}), 6);
+		ResultCheck.check(main.evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}), 22);
+		ResultCheck.check(main.evalRPN(new String[]{"4", "3", "-"}), 1);
 	}
 }
