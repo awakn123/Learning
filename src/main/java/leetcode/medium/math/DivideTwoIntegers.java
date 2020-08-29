f6d8d7f93dfc712c2cf969928c821dcc5e5101e9
awakn123 <awakn123@gmail.com>
2020-08-29 16:13:03 +0800

    #leetcode# medium dividetwointegers.

--- src/main/java/leetcode/medium/math/DivideTwoIntegers.java
+++ src/main/java/leetcode/medium/math/DivideTwoIntegers.java
@@ -9,12 +9,44 @@ import leetcode.util.ResultCheck;
  */
 public class DivideTwoIntegers {
 	public int divide(int dividend, int divisor) {
-		return 0;
+		if (dividend == 0) return 0;
+		if (divisor == 1) return dividend;
+		if (divisor == -1) {
+			if (dividend>Integer.MIN_VALUE)
+				return -dividend;
+			else
+				return Integer.MAX_VALUE;
+		}
+		long dd = (long) dividend, ds = (long) divisor;
+		if (dd < 0) dd = -dd;
+		if (ds < 0) ds = -ds;
+		if (dd < ds) return 0;
+		long r = 0, dsOri = ds;
+		while (true) {
+			int cal = 1;
+			ds = dsOri;
+			while (dd > (ds + ds)) {
+//				ds += ds;
+//				cal += cal;
+				ds = ds << 1;
+				cal = cal << 1;
+			}
+			r += cal;
+			if (dd >= ds && dd < ds + dsOri)
+				break;
+			dd -= ds;
+		}
+		if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+			return (int)-r;
+		else
+			return (int)r;
 	}
 
 	public static void main(String[] args){
 		DivideTwoIntegers main = new DivideTwoIntegers();
 		ResultCheck.check(main.divide(10, 3), 3);
 		ResultCheck.check(main.divide(7, -3), -2);
+		ResultCheck.check(main.divide(-2147483648, -1), 2147483647);
+		ResultCheck.check(main.divide(2, 2), 1);
 	}
 }
