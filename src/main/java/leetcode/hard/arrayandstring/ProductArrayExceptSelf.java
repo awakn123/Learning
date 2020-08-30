ecb9a3d05b9cceb7d897490344a7238944034b03
awakn123 <awakn123@gmail.com>
2020-08-30 16:19:38 +0800

    #leetcode# hard product of array except self

--- src/main/java/leetcode/hard/arrayandstring/ProductArrayExceptSelf.java
+++ src/main/java/leetcode/hard/arrayandstring/ProductArrayExceptSelf.java
@@ -1,5 +1,7 @@
 package leetcode.hard.arrayandstring;
 
+import leetcode.util.ResultCheck;
+
 /**
  * Created by 曹云 on 2020/8/30.
  * 238. 除自身以外数组的乘积
@@ -7,6 +9,22 @@ package leetcode.hard.arrayandstring;
  */
 public class ProductArrayExceptSelf {
 	public int[] productExceptSelf(int[] nums) {
-		return null;
+		int[] res = new int[nums.length];
+		int multiple = 1;
+		for (int i=0;i<nums.length;i++) {
+			res[i] = multiple;
+			multiple *= nums[i];
+		}
+		multiple = 1;
+		for (int i=nums.length-1; i>=0; i--) {
+			res[i] *= multiple;
+			multiple *= nums[i];
+		}
+		return res;
+	}
+
+	public static void main(String[] args){
+		ProductArrayExceptSelf main = new ProductArrayExceptSelf();
+		ResultCheck.check(main.productExceptSelf(new int[]{1,2,3,4}), new int[]{24,12,8,6});
 	}
 }
