a73e8df9f596af2ef40a3b59e84ec3bf0165af2a
awakn123 <awakn123@gmail.com>
2020-08-31 21:31:25 +0800

    #leetcode# hard first missing positive.

--- src/main/java/leetcode/hard/arrayandstring/FirstMissingPositive.java
+++ src/main/java/leetcode/hard/arrayandstring/FirstMissingPositive.java
@@ -10,7 +10,18 @@ import leetcode.util.ResultCheck;
 public class FirstMissingPositive {
 
 	public int firstMissingPositive(int[] nums) {
-		return 0;
+		for (int i=0;i<nums.length;i++) {
+			while(nums[i]>0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+				int tmp = nums[nums[i] - 1];
+				nums[nums[i] - 1] = nums[i];
+				nums[i] = tmp;
+			}
+		}
+		for (int i = 0; i<nums.length; i++) {
+			if (nums[i] != i + 1)
+				return i+1;
+		}
+		return nums.length + 1;
 	}
 
 	public static void main(String[] args){
