bdf00d15561ed3c1cedbf2e247b9250a30eb5cc8
awakn123 <awakn123@gmail.com>
2020-09-01 09:16:34 +0800

    #leetcode# hard find duplicate number.

--- src/main/java/leetcode/hard/arrayandstring/FindDuplicateNumber.java
+++ src/main/java/leetcode/hard/arrayandstring/FindDuplicateNumber.java
@@ -2,6 +2,8 @@ package leetcode.hard.arrayandstring;
 
 import leetcode.util.ResultCheck;
 
+import java.util.Arrays;
+
 /**
  * Created by 曹云 on 2020/8/30.
  * 287. 寻找重复数
@@ -9,12 +11,24 @@ import leetcode.util.ResultCheck;
  */
 public class FindDuplicateNumber {
 	public int findDuplicate(int[] nums) {
-		return 0;
+		int slow = 0, fast = 0;
+		do {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		} while (slow != fast);
+		slow = 0;
+		while (slow != fast) {
+			slow = nums[slow];
+			fast = nums[fast];
+		}
+		return slow;
 	}
 
 	public static void main(String[] args){
 		FindDuplicateNumber main = new FindDuplicateNumber();
 		ResultCheck.check(main.findDuplicate(new int[]{1,3,4,2,2}), 2);
 		ResultCheck.check(main.findDuplicate(new int[]{3,1,3,4,2}), 3);
+		ResultCheck.check(main.findDuplicate(new int[]{2,5,9,6,9,3,8,9,7,1}), 9);
+		ResultCheck.check(main.findDuplicate(new int[]{2,1,2}), 2);
 	}
 }
