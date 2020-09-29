3862db2adf0d62c5394feb26a665308ce7ee107a
awakn123 <awakn123@gmail.com>
2020-09-30 07:49:48 +0800

    #leetcode# hard word break.

--- src/main/java/leetcode/hard/dynamic/WordBreak.java
+++ src/main/java/leetcode/hard/dynamic/WordBreak.java
@@ -23,34 +23,22 @@ public class WordBreak {
 			trie.addWord(word);
 			maxLength = Math.max(maxLength, word.length());
 		}
-		int start = 0;
-		while(start < chars.length) {
-			int nextStart = findNextStart(chars, trie, start);
-			int l = 0;
-			while (nextStart <= start && l < maxLength && l < start - 1) {
-				l++;
-				if (dp[start - l - 1]) {
-					int k = start - l, nextK = k;
-					do {
-						k = nextK;
-						nextK = findNextStart(chars, trie, k);
-					} while(nextK > k);
-					nextStart = nextK;
-				}
+
+		check(chars, trie, 0);
+		for (int i=0; i<dp.length;i++) {
+			if (dp[i]) {
+				check(chars, trie, i+1);
 			}
-			if (nextStart <= start)
-				return false;
-			start = nextStart;
 		}
+
 		return dp[chars.length - 1];
 	}
 
-	private int findNextStart(char[] chars, Trie trie, int start) {
+	private void check(char[] chars, Trie trie, int start) {
 		if (start < 0 || start >= chars.length)
-			return start;
+			return;
 		Trie node = trie;
 		int i = start;
-		int nextStart = start;
 		while (node != null && i < chars.length) {
 			char c = chars[i];
 			int idx = c - 'a';
@@ -58,12 +46,10 @@ public class WordBreak {
 			if (node != null) {
 				if (node.isEnd) {
 					dp[i] = true;
-					nextStart = i + 1;
 				}
 				i++;
 			}
 		}
-		return nextStart;
 	}
 
 	class Trie{
