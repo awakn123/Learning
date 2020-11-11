44869710977b104aca4bdb7b2bcd6dd94b57d775
awakn123 <awakn123@gmail.com>
2020-11-12 04:42:59 +0800

    #web# CodingMan and Scheduler.

---
+++ src/main/web/interview/write/CodingMan.js
@@ -0,0 +1,80 @@
+function CodingMan(name) {
+	function Man(name) {
+		this.name = name;
+		setTimeout(() => {
+			console.log(`Hi! This is ${name}!`);
+		}, 0)
+	}
+
+	Man.prototype.sleep = function(time){
+		let curTime = new Date();
+		setTimeout(() => {
+			while (new Date() - curTime < time * 1000) {}
+			console.log(`Wake up after ${time}`);
+		}, 0)
+		return this;
+	}
+
+	Man.prototype.sleepFirst = function(time) {
+		let curTime = new Date();
+		while(new Date() - curTime < time * 1000) {}
+		console.log(`Wake up after ${time}`);
+		return this;
+	}
+
+	Man.prototype.eat = function(food){
+		setTimeout(() => console.log(`Eat ${food}~`), 0);
+		return this;
+	}
+
+	return new Man(name);
+}
+
+CodingMan("Hank");
+//Hi! This is Hank!
+CodingMan("Hank").sleep(10).eat("dinner");
+//Hi! This is Hank!
+//10s
+//Wake up after 10
+//Eat dinner~
+CodingMan("Hank").sleepFirst(5).eat("dinner").eat("supper");
+// 5s
+//Wake up after 5
+//Hi! This is Hank!
+//	Eat dinner~
+//	Eat supper~
+
+// -------------number 2---------------
+function CodingMan(name) {
+	function Man(name) {
+		this.name = name;
+		this.sleepTime = 0;
+		setTimeout(() => {
+			console.log(`Hi! This is ${name}!`);
+		}, 0)
+	}
+
+	Man.prototype.sleep = function(time){
+		this.sleepTime = time;
+		let curTime = new Date();
+		setTimeout(() => {
+			while (new Date() - curTime < time * 1000) {}
+			console.log(`Wake up after ${time}`);
+		}, this.sleepTime)
+		return this;
+	}
+
+	Man.prototype.sleepFirst = function(time) {
+		let curTime = new Date();
+		while(new Date() - curTime < time * 1000) {}
+		console.log(`Wake up after ${time}`);
+		return this;
+	}
+
+	Man.prototype.eat = function(food){
+		setTimeout(() => console.log(`Eat ${food}~`), this.sleepTime);
+		return this;
+	}
+
+	return new Man(name);
+}
---
+++ src/main/web/interview/write/Scheduler.js
@@ -0,0 +1,28 @@
+function Scheduler(num) {
+	this.list = [];
+	this.count = 0;
+	this.add = async function(fn) {
+		(this.count >= num) && await new Promise((resolve) => this.list.push(resolve));
+		this.count++;
+		let result = await fn();
+		this.count--;
+		if (this.list.length > 0) {
+			this.list.shift()();
+		}
+		return result;
+	}
+}
+
+function delay (key, time) {
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(key), time)
+	});
+}
+
+var sc = new Scheduler(2);
+var startTime = new Date().getTime();
+sc.add(() => delay(1, 1000)).then((key) => console.log(`${key}:${new Date().getTime() - startTime}`));
+sc.add(() => delay(2, 1000)).then((key) => console.log(`${key}:${new Date().getTime() - startTime}`));
+sc.add(() => delay(3, 1000)).then((key) => console.log(`${key}:${new Date().getTime() - startTime}`));
+sc.add(() => delay(4, 1000)).then((key) => console.log(`${key}:${new Date().getTime() - startTime}`));
+sc.add(() => delay(5, 1000)).then((key) => console.log(`${key}:${new Date().getTime() - startTime}`));
