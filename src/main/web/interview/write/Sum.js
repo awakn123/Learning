8a8ed4bf6d302b6dcb48d62c0f25134e06adb6b6
awakn123 <awakn123@gmail.com>
2020-11-12 05:01:36 +0800

    #web# Sum

---
+++ src/main/web/interview/write/Sum.js
@@ -0,0 +1,19 @@
+//实现 sum(1)(2)(3).valueOf()
+//支持 sum(1),sum(1)(2,3)(4),sum(1,2,3,4)
+
+function sum() {
+	let args = Array.from(arguments);
+	let sum = args.reduce((sum,v) => sum+=v);
+	function add() {
+		let args = Array.from(arguments);
+		sum += args.reduce((sum,v) => sum+=v);
+		return add;
+	}
+	add.valueOf = add.toString = function(){
+		return sum;
+	}
+	return add;
+}
+
+
+//实现 taskSum(1000,()=>{console.log(1)}).task(1200,()=>{console.log(2)}).task(1300,()=>{console.log(3)})， 这里等待 1s，打印 1，之后等待 1.2s，打印 2，之后打印 1.3s，打印 3
