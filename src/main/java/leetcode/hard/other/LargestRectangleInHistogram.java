01ca8e4bd3f09247191db8e5ba368b87bf477f98
awakn123 <awakn123@gmail.com>
2020-11-13 10:50:57 +0800

    #web# Largest Rectangle In Histogram.

--- src/main/java/leetcode/hard/other/LargestRectangleInHistogram.java
+++ src/main/java/leetcode/hard/other/LargestRectangleInHistogram.java
@@ -2,6 +2,8 @@ package leetcode.hard.other;
 
 import leetcode.util.ResultCheck;
 
+import java.util.Stack;
+
 /**
  * Created by 曹云 on 2020/11/12.
  * 84. 柱状图中最大的矩形
@@ -9,10 +11,26 @@ import leetcode.util.ResultCheck;
  */
 public class LargestRectangleInHistogram {
 	public int largestRectangleArea(int[] heights) {
+		int[] left = new int[heights.length];
+		int[] right = new int[heights.length];
+		Stack<Integer> stack = new Stack<>();
 		for (int i=0; i<heights.length; i++) {
-
+			while(!stack.isEmpty() && heights[stack.peek()] > heights[i]) {
+				int k = stack.pop();
+				right[k] = i - k;
+			}
+			left[i] = stack.isEmpty() ? (i + 1) : (i - stack.peek());
+			stack.push(i);
 		}
-		return 0;
+		while (!stack.isEmpty()) {
+			int i = stack.pop();
+			right[i] = heights.length - i;
+		}
+		int max = 0;
+		for (int i=0; i<heights.length; i++) {
+			max = Math.max(heights[i] * (left[i] + right[i] - 1), max);
+		}
+		return max;
 	}
 
 	public static void main(String[] args){
