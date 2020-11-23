5a11f711e89f37745a3d98809350ccdd5bbe2421
awakn123 <awakn123@gmail.com>
2020-11-24 05:46:30 +0800

    #web# CodingMan Question

--- src/main/web/interview/review1124/CodingMan.js
+++ src/main/web/interview/review1124/CodingMan.js
@@ -1,3 +1,29 @@
+/**
+ 题目描述：实现一个CodingMan，可以按照以下方式调用：
+ CodingMan(‘Hank’)
+ 输出：
+ Hi！This is Hank!
+
+ CodingMan(‘Hank’).sleep(10).eat(‘dinner’)
+ 输出：
+ Hi！This is Hank!
+ //等待10秒
+ Wake up after 10
+ Eat dinner~
+
+ CodingMan(‘Hank’).eat(‘dinner’).eat(‘supper’)
+ 输出：
+ Hi！This is Hank!
+ Eat dinner~
+ Eat supper~
+
+ CodingMan(‘Hank’).sleepFirst(5).eat(‘supper’)
+ 输出：
+ //等待5秒
+ Wake up after 5
+ Hi！This is Hank!
+ Eat supper~
+ */
 function CodingMan(name) {
 	function Man(name) {
 		this.name = name;
