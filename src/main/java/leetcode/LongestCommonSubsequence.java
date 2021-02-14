c24a4863f6673cd466c5f525d98d070c6199bf80
awakn123 <awakn123@gmail.com>
2021-02-14 16:52:43 +0800

    #leetcode# init lcs.

---
+++ src/main/java/leetcode/LongestCommonSubsequence.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * Created by 曹云 on 2021/2/14.
+ * https://leetcode-cn.com/problems/longest-common-subsequence/
+ */
+public class LongestCommonSubsequence {
+	public int longestCommonSubsequence(String text1, String text2) {
+		return 0;
+	}
+}
---
+++ src/test/leetcode/LongestCommonSubsequenceTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by 曹云 on 2021/2/14.
+ */
+public class LongestCommonSubsequenceTest {
+	@Test
+	void test(){
+		LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+		Assertions.assertEquals(3, lcs.longestCommonSubsequence("abcde", "ace"));
+		Assertions.assertEquals(3, lcs.longestCommonSubsequence("abc", "abc"));
+		Assertions.assertEquals(0, lcs.longestCommonSubsequence("abc", "def"));
+	}
+}
